b5332ce24067f60c17858505d6349f4cb990787e
pujaraansh <ansh@griffithuni.edu.au>
2023-09-14 15:41:43 +1000

    Que done

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,34 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
 
+# Connect to the database
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+# Fetch the required values (assuming table and column names; you should replace them with actual names)
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+data = cursor.fetchall()
+
+# Close the database connection
+conn.close()
+
+# Populate the Python lists with the fetched values
+years = [row[0] for row in data]
+co2 = [row[1] for row in data]
+temp = [row[2] for row in data]
+
+# Plotting the data
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+
+# Save the figure before displaying it
+plt.savefig("co2_temp_1.png")
+plt.show()
